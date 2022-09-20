0d5aec74171e52c99166a5d90c45608042770bdf
Ankita7372 <105856249+Ankita7372@users.noreply.github.com>
2022-09-20 11:44:41 +0530

    python activities

---
+++ Activity21.py
@@ -0,0 +1,46 @@
+
+def test_addition():
+
+
+    num1 = 10
+    num2 = 15
+
+    sum = num1 + num2
+
+    assert sum == 25
+
+def test_subtraction():
+
+    # Initialize two numbers
+    num1 = 50
+    num2 = 35
+
+    # Subtract them
+    diff = num1 - num2
+
+    # Assertion
+    assert diff == 15
+
+# Multiplication test
+def test_multiplication():
+
+    # Initialize two numbers
+    num1 = 5
+    num2 = 20
+
+    # Multiply them
+    prod = num1 * num2
+
+    # Assertion
+    assert prod == 100
+
+
+def test_division():
+
+    num1 = 100
+    num2 = 5
+
+
+    quot = num1 / num2
+
+    assert quot == 20
